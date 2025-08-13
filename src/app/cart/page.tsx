cd4fc5d778de5bded276b72cdc6d3b183f6e3258
Ertannur Atabay <191880604+Ertannur@users.noreply.github.com>
2025-08-13 10:52:33 +0300

    cartStore import yolu düzeltildi

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { useCartStore } from "../stores/cartStore";
+import { useCartStore } from "../../stores/cartStore";
 
 const CartPage = () => {
   const cartItems = useCartStore((state) => state.items);
